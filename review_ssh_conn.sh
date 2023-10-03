fffaee76b9092b58a12ce402a36dee94e0b6d1c3
arturo.robles.vargas <aroblesv@login01.lab10b2.deacluster.intel.com>
2023-10-02 18:11:57 -0700

    add new review_ssh_conn

--- review_ssh_conn.sh
+++ review_ssh_conn.sh
@@ -1,15 +1,67 @@
 #!/bin/bash
 
-#Parameters
-: "${1?Must provide node name}"
-NODE=${1}
+function review_ssh {
+
+NODE=($1);
 port=22
 connect_timeout=5
 
-timeout $connect_timeout bash -c "</dev/tcp/$NODE/$port"
+timeout ${connect_timeout} bash -c "</dev/tcp/${NODE[0]}/${port}"
 
 if [ $? == 0 ];then
-   echo -e "\r\033[0mSSH Connection to $NODE | \033[32mssh_ok\033[0m\r"
+   echo -e "${NODE[0]} \033[32mssh_live\033[0m" >> report_ssh
 else
-   echo -e "\r\033[0mSSH connection to $NODE | \033[33mssh_fail\033[0m\r"
+   echo -e "${NODE[0]} \033[33mssh_dead\033[0m" >> report_ssh
 fi
+}
+
+read -p "type the list: " list
+echo -e "\nGenerating ssh connection information for the netowork and for the BMC, wait a moment, please. \n"
+NODES=$(grep "zp31" ${list})
+
+if [ -z "${NODES}" ]; then
+	echo "empty list"
+	exit 1
+fi
+
+for n in ${NODES}; do
+	review_ssh $n
+done
+
+function review_ssh_bmc {
+NODE=($1);
+port=22
+connect_timeout=5
+
+timeout $connect_timeout bash -c "</dev/tcp/${NODE[0]}/${port}"
+
+if [ $? == 0 ]; then
+   echo -e "${NODE[0]} \033[32mssh-bmc_live\033[0m" >> report_sshbmc
+else
+   echo -e "${NODE[0]} \033[33mssh-bmc_dead\033[0m" >> report_sshbmc
+fi
+}
+
+listb=${list}bmc
+
+NODES=$(grep "zp31" $listb)
+
+if [ -z "${NODES}" ]; then
+	echo "empty list"
+	exit 1
+fi
+
+for n in ${NODES}; do
+	review_ssh_bmc $n
+done
+
+tr 's' 'b' < $list > ${list}bmc
+sed -e 's/$/\.deacluster.intel.com/' ${list}bmc > webviewss
+sed -i 's/^/https:\/\//g' webviewss
+
+paste report_ssh report_sshbmc > ssh_status_report
+cat ssh_status_report
+echo -e "\n*******************************************************************************************************"
+echo -e "copy and pastate into an excel sheet to be able to enter the nodes with which you want to work\n"
+cat webviewss
+rm report_ssh report_sshbmc
