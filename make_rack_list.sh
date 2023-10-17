7a1ca001210f0700c73aa2876582e394f966470a
arturo.robles.vargas <aroblesv@login01.lab10b2.deacluster.intel.com>
2023-10-16 17:47:45 -0700

    rack_ssh_status

---
+++ make_rack_list.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+read -p "Write number rack sequence: " racknumb
+
+if [[ $racknumb =~ ^[0-9]+$ ]]; then
+
+	for n in {1..20}; do echo "zp3110b001s$racknumb"; done > racksequence1
+
+	seq -w 1 20 > racksequence2
+
+	paste racksequence1 racksequence2 > racksequence
+
+	sed -i 's/[[:blank:]]//g' racksequence
+
+	cat racksequence
+
+	./review_ssh_rack_status.sh
+
+else
+	rm -rf *racksequence*
+
+	for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
+
+	do
+		for n in {1..20}; do echo "zp3110b001s$i"; done >> racksequence1
+
+		seq -w 1 20 >> racksequence2
+		
+		paste racksequence1 racksequence2 >> temp_racksequence
+
+		sed -i 's/[[:blank:]]//g' temp_racksequence
+		
+
+		sort temp_racksequence | uniq > racksequence
+		cat racksequence
+		#./review_ssh_rack_status.sh
+	done
+
+fi
