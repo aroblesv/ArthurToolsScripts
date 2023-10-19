97b61a608df0a6c2b0a6d57e73b9a33eec59eea0
arturo.robles.vargas <aroblesv@login01.lab10b2.deacluster.intel.com>
2023-10-19 16:09:53 -0700

    update downgrade 9.30 to 8.10

--- update_3.0_fortville.sh
+++ update_3.0_fortville.sh
@@ -112,7 +112,16 @@ if [[ "$EXPECTED_FW" < "$CURRENT_FW" ]]; then
 		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMDowngradePackage_v9_20_to_v8_10_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
 		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
 		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
+	elif [[ "$CURRENT_FW" = "9.30" ]]; then
+		echo "downloading system firmware to $EXPECTED_FW"
+		s3web_url="$(ping -c2 s3web | grep statistics | sed 's/.*s3web/s3web/g' | awk '{ printf $1 }')"
+  		ssh root@${NODE_ID} "curl -LO http://${s3web_url}/bkc-mirror/wilson_city/network_fw/700Series_NVMDowngradePackage_v9_30_to_v8_10_Linux.tar.gz"
+		#scp 700Series_NVMUpdatePackage_v9_20_Linux.tar.gz root@${NODE_ID}:/root/
+		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMDowngradePackage_v9_30_to_v8_10_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
+		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
+		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
 	fi
+
 else
 echo the system has the same firmware, nothing to do!!!
 fi
