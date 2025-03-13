cb98ea1c190f8147c06076e6946b6e9a6a962a0d
arturo.robles.vargas <aroblesv@login01.lab10b2.deacluster.intel.com>
2025-03-12 18:32:46 -0700

    update find system outlet script

--- find_system_outlet_row2.sh
+++ find_system_outlet_row2.sh
@@ -8,6 +8,10 @@ read -p "PDU: " npdu
 echo -e "\033[33mEnter the OUTLET number with this format "X" if the number is of 1 digit.\033[0m"
 read -p "OUTLET: " noutlet
 
-nodename=$(grep zp3110b002s$nrack /var/local/Sysman/cluster_nodes.py |grep p$nrack$npdu|grep \"$noutlet\"\)\,$ |tr ',' ' '|awk '{print "system:"$5 "\turl-check-pdu:"$7 "\toutlet:"$8}')
+nodename=$(grep zp3110b002s$nrack /var/local/Sysman/cluster_nodes.py |grep p$nrack$npdu|grep \"$noutlet\"\)\,$ |tr ',' ' '|awk '{print "system:"$1 "\turl-check-pdu:"$7 "\toutlet:"$8 "\tsystemoff_command:sysman --pdu_off -M "$1}')
 echo -e "***********************************************************************************"
-echo -e "\n\033[32m$nodename\033[0m\n"
+echo -e "$nodename" > nodename_temp2
+nodename2=$(awk '{gsub(/"/,"")}1' nodename_temp2)
+echo -e "$nodename2" > nodename_temp3
+nodename3=$(awk '{gsub(/)/,"")}1' nodename_temp3)
+echo -e "\n\033[32m$nodename3\033[0m\n"
