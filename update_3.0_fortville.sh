4210d2fc832e46694e6b2e52575190f1694f7b7e
arturo.robles.vargas <aroblesv@login01.lab10b2.deacluster.intel.com>
2025-07-02 00:39:01 -0700

    update nic 9.54

--- update_3.0_fortville.sh
+++ update_3.0_fortville.sh
@@ -96,6 +96,33 @@ if [[ "$EXPECTED_FW" > "$CURRENT_FW" ]]; then
 		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMUpdatePackage_v9_52_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
 		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
 		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
+
+        elif [[ "$EXPECTED_FW" = "9.52" ]]; then
+		echo "updating to $EXPECTED_FW version"
+		#s3web_url="$(ping -c2 s3web | grep statistics | sed 's/.*s3web/s3web/g' | awk '{ printf $1 }')"
+		#ssh root@${NODE_ID} "curl -LO --noproxy '*' http://${s3web_url}/download/bkc-mirror/wilson_city/network_fw/700Series_NVMUpdatePackage_v9_40_Linux.tar.gz"
+		scp 700Series_NVMUpdatePackage_v9_52_Linux.tar.gz root@${NODE_ID}:/root/
+		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMUpdatePackage_v9_52_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
+		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
+		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
+
+        elif [[ "$EXPECTED_FW" = "9.53" ]]; then
+		echo "updating to $EXPECTED_FW version"
+		#s3web_url="$(ping -c2 s3web | grep statistics | sed 's/.*s3web/s3web/g' | awk '{ printf $1 }')"
+		#ssh root@${NODE_ID} "curl -LO --noproxy '*' http://${s3web_url}/download/bkc-mirror/wilson_city/network_fw/700Series_NVMUpdatePackage_v9_40_Linux.tar.gz"
+		scp 700Series_NVMUpdatePackage_v9_53_Linux.tar.gz root@${NODE_ID}:/root/
+		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMUpdatePackage_v9_53_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
+		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
+		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
+
+        elif [[ "$EXPECTED_FW" = "9.54" ]]; then
+		echo "updating to $EXPECTED_FW version"
+		#s3web_url="$(ping -c2 s3web | grep statistics | sed 's/.*s3web/s3web/g' | awk '{ printf $1 }')"
+		#ssh root@${NODE_ID} "curl -LO --noproxy '*' http://${s3web_url}/download/bkc-mirror/wilson_city/network_fw/700Series_NVMUpdatePackage_v9_40_Linux.tar.gz"
+		scp 700Series_NVMUpdatePackage_v9_54_Linux.tar.gz root@${NODE_ID}:/root/
+		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMUpdatePackage_v9_54_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
+		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
+		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
 	fi
 fi
 
@@ -163,6 +190,24 @@ if [[ "$EXPECTED_FW" < "$CURRENT_FW" ]]; then
   		#ssh root@${NODE_ID} "curl -LO --noproxy '*' http://${s3web_url}/download/bkc-mirror/wilson_city/network_fw/700Series_NVMDowngradePackage_v9_40_to_v8_10_Linux.tar.gz"
 		scp 700Series_NVMDowngradePackage_v9_50_to_v8_10_Linux.tar.gz root@${NODE_ID}:/root/
 		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMDowngradePackage_v9_50_to_v8_10_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
+		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
+		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
+
+	elif [[ "$CURRENT_FW" = "9.53" ]]; then
+		echo "downloading system firmware to $EXPECTED_FW"
+		#s3web_url="$(ping -c2 s3web | grep statistics | sed 's/.*s3web/s3web/g' | awk '{ printf $1 }')"
+  		#ssh root@${NODE_ID} "curl -LO --noproxy '*' http://${s3web_url}/download/bkc-mirror/wilson_city/network_fw/700Series_NVMDowngradePackage_v9_40_to_v8_10_Linux.tar.gz"
+		scp 700Series_NVMDowngradePackage_v9_53_to_v8_10_Linux.tar.gz root@${NODE_ID}:/root/
+		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMDowngradePackage_v9_53_to_v8_10_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
+		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
+		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
+
+	elif [[ "$CURRENT_FW" = "9.54" ]]; then
+		echo "downloading system firmware to $EXPECTED_FW"
+		#s3web_url="$(ping -c2 s3web | grep statistics | sed 's/.*s3web/s3web/g' | awk '{ printf $1 }')"
+  		#ssh root@${NODE_ID} "curl -LO --noproxy '*' http://${s3web_url}/download/bkc-mirror/wilson_city/network_fw/700Series_NVMDowngradePackage_v9_40_to_v8_10_Linux.tar.gz"
+		scp 700Series_NVMDowngradePackage_v9_54_to_v8_10_Linux.tar.gz root@${NODE_ID}:/root/
+		ssh root@${NODE_ID} "rm -rf 700Series; tar -xvf 700Series_NVMDowngradePackage_v9_54_to_v8_10_Linux.tar.gz; cd 700Series/Linux_x64; chmod 777 nvmupdate64e; 
 		chmod 777 nvmupdate.cfg; ./nvmupdate64e -u -l -o update.xml -c nvmupdate.cfg"
 		ssh root@${NODE_ID} "rm -rf 700Series; reboot 0"
 	fi
